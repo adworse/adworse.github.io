c9ab13e32fb189adec3f49230ef1a35caed88ca6
Vladimir Ermilov <adworse@MBA.local>
2017-12-25 19:09:45 +0530

    AskDandrew w/o users

--- comms_sample_files/private_messages_user_show.js
+++
@@ -1,9 +0,0 @@
-jQuery(function(){
-  ich.refresh();
-  var newMessageView = new NewMessageView({el: $('.message-area')});
-  $('.send-new-message').click(function(e){
-    e.preventDefault();
-    newMessageView.show();
-  });
-});
-
